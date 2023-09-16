2aa512de14b38261d96c65e06dbae9d296cdc76d
yoimaral <yalozano9@misena.edu.co>
2023-09-16 06:45:42 -0500

    Segunda prueba casi terminada con programacion funcional

--- retoNebula.js
+++ retoNebula.js
@@ -25,4 +25,55 @@ let consultation = conection.query(
   }
 );
 
+/**SEGUNDO EJERCICIO con SQL. Para ejecutarlo se elimina des de conecton hasta la impresión de result*/
+/**
+conection.query(
+    "SELECT Founded, MIN(Number_of_employees) AS MinEmployees, MAX(Number_of_employees) AS MaxEmployees, AVG(Number_of_employees) AS AvgEmployees FROM provider WHERE Founded BETWEEN 1985 AND 1995 GROUP BY Founded; ",
+    (err, rows) => {
+      if (err) throw err;
+      console.table("Los datos de la tabla son estos");
+      console.table(rows);
+ */
+
+/* Query a la base de datos*/
+conection.query(
+  "SELECT  Number_of_employees, founded FROM provider ",
+  (err, rows) => {
+    if (err) throw err;
+    console.table("Los datos de la tabla son estos");
+    /* Filtro entre las fechas*/
+    let filterCompanies = rows.filter(
+      (e) => e.founded >= 1985 && e.founded <= 1995
+    );
+
+    /* Aqui se filtro de compañias*/
+    let groupByYears = filterCompanies.reduce((acumulador, company) => {
+      const foundedYear = company.founded;
+      if (!acumulador[foundedYear]) {
+        acumulador[foundedYear] = [];
+      }
+
+      acumulador[foundedYear].push(company.Number_of_employees);
+      return acumulador;
+    });
+
+    const result = Object.keys(groupByYears).map((year) => {
+      const employeeNumbers = groupByYears[year];
+      const minEmployees = Math.min(...employeeNumbers);
+      const maxEmployees = Math.max(...employeeNumbers);
+      const avgEmployees =
+        employeeNumbers.reduce((sum, value) => sum + value, 0) /
+        employeeNumbers.length;
+
+      return {
+        founded_year: parseInt(year, 10),
+        min_employees: minEmployees,
+        max_employees: maxEmployees,
+        avg_employees: avgEmployees,
+      };
+    });
+    console.table(result);
+  }
+);
+
 conection.end();
